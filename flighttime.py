4c572bae18c19be83081cae2f326ed857d0f63a5
Robert Jensen <jensen.robert@gmail.com>
2011-12-13 21:54:09 +0100

    Initial code clean-up, now with consistent comments and slightly better variable handling

--- flighttime.py
+++ flighttime.py
@@ -1,51 +1,86 @@
 
 #pos=0 is defined to be at the center axis of the incoming ions
 def field(pos):
-    #dR1ZGap = 80 # Not correct distance
-
-    #Measured from CAD drawing
-    #A1-A2: 1.333cm
-    #A1-A3: 1.333cm
-
+    """
+    Calculates the electric field at a given position in the instrument.
+    All distances are measured from CAD drawing.
+    The result is given in V/cm.
+    
+    Args:
+        pos: the position in cm in the instrument. pos=0 is defined to be at the center axis of the incoming ions
+    
+    Returns:
+        Tuple containing field and distance until it needs to be checked again in either direction
+        
+    Raises:
+        ....
+    """
+        
     Params = []
-    #Params.append((-2.5,Pulse_Voltage))    #A1
-    #Params.append((2.5,0))                 #A2
-    Params.append((-0.635,Pulse_Voltage))   #A1
-    Params.append((0.635,0))                #A2
-    Params.append((1.9685,Liner_Voltage))   #A3
-    Params.append((95.235,Liner_Voltage))   #Liner grid
-    Params.append((96.390,R1_Voltage))      #R1
-    Params.append((110.81,R2_Voltage))      #R2
-    Params.append((10000,R2_Voltage))       #This field extends to infinity
+    Params.append((-0.635,Voltages['pulse']))   #A1
+    Params.append((0.635,0))                    #A2
+    Params.append((1.9685,Voltages['liner']))   #A3
+    Params.append((95.235,Voltages['liner']))   #Liner grid
+    Params.append((96.390,Voltages['R1']))      #R1
+    Params.append((110.81,Voltages['R2']))      #R2
+    Params.append((10000,Voltages['R2']))       #This field extends to infinity
 
     i = 0
     while Params[i][0] < pos:
         i = i+1
     field = (Params[i][1] - Params[i-1][1]) / (Params[i][0] - Params[i-1][0])
 
-    return (field,Params[i][0],Params[i-1][0]) #Tuple containing field (Volt / cm) and
-                                               #distance until it needs to be checked again in either direction
+    return (field,Params[i][0],Params[i-1][0]) #
 
-# F = q*E
-# A = F / m
+
 def acceleration(field,mass):
-    elementary_charge = -1.602e-19 # Unit, columb
-    force = field * elementary_charge * 100 # Unit, N, factor of 100 compensating for field unit of V/cm
-    acc = force / (mass * 1.6605e-27) #Unit, m/s, mass unit AMU, thus compensating factor
+    """
+    Calculates the acceleration of a given mass in a given field if it is
+    singly ionized
+    Acceleration calculated as A = q*E/m, q = -e
+    
+    Args:
+        field: The electrical field, given in V/cm
+        mass: The mass in AMU
+        
+    Returns:
+        The acceleration in m/s^2
+    
+    Raises:
+        ...
+    """
+    elementary_charge = -1.602e-19
+    force = field * elementary_charge * 100
+    acc = force / (mass * 1.6605e-27)
     return acc
 
 
 
 def flight_time(mass,pos=0):
+    """
+    Calculates total flight time of an ion of given mass
+    This function may need some tidying up.
+    
+    Args:
+        mass: Mass of the ion
+        pos: Initial position, default is mid between A1 and A2
+        
+    Returns:
+        t: The actual flighttime
+        status_text: ....
+        values: ...
+        
+    Raises:
+        ...
+    """
     t = 0
     dt = 1e-10
     dT = dt
-    v = 1e-10 # Make sure v is positive from the beginning
+    v = dt # Just to make sure v is positive from the beginning
     i = 0
     valid_until = -100000
-    t_values = [] #Aaarrgghhh
-    v_values = [] #Aaaaarrgggghhhh
-    pos_values = [] #This need to be done as a single list of tuples!!!!
+    values = {} #Named list containing various status-information
+    values['time'] = values['speed'] = values['pos'] = []
     while (((pos < 110.8) & (v>0)) | ((pos > 37.167) & (v<0))):
         if v>0:
             if pos > valid_until:
@@ -73,28 +108,30 @@ def flight_time(mass,pos=0):
         t = t+dT
         i = i+1
         if (i%100 == 0):
-            t_values.append(t*1e6)
-            pos_values.append(pos)
-            v_values.append(v)
-            #print pos
-            #print E
-            #print field(pos)
-            #print t
-            #print pos
-            #print A
-            #print v
-            #print "--"
+            values['time'].append(t*1e6)
+            values['pos'].append(pos)
+            values['speed'].append(v)
 
 
     s1 =  "Time {0:.3f} microseconds".format(t*1e6)
     s2 =  "Speed: {0:.2f} km/s".format(v / 1000)
     s3 =  "Distance error: {0}".format(pos-107.5)
     status_text = s1 + "\n" + s2 + "\n" + s3
-    return (t,status_text,t_values,pos_values,v_values)
+    return (t,status_text,values)
 
 
-"""Create a graph for expected flighttimes"""
+
 def draw_trajectory(mass):
+    """
+    Create a graph for expected flighttimes
+    
+    Args:
+        mass: The mass to be calculated
+    
+    Returns:
+    
+    Raises:
+    """
     res =  flight_time(mass,0)
     res_slow =  flight_time(mass,SLOW_POS)
     res_fast =  flight_time(mass,FAST_POS)
@@ -109,8 +146,19 @@ def draw_trajectory(mass):
 
 
 
-"""Print flight times of various masse"""
+
 def print_flighttimes(html, print_values,export_figure):
+    """
+    Print flight times of various masse
+    Args:
+        html: If true, the output will be formatted for a web-browser
+        print_values...
+        export_figure...
+        
+    Returns:
+    
+    Raises:
+    """
     flight_times = []
     masses = []
     for mass in range(1,50): 
@@ -149,27 +197,21 @@ parser = optparse.OptionParser('Calculates expected flighttime in the TOF')
 parser.add_option('--html', action="store_true", default=False, help='Adds html line breaks to the text output')
 parser.add_option('--liner', '-L', action="store", default=-2000, type=int, help='Liner Voltage')
 parser.add_option('--pulse', '-P', action="store", default=400, type=int, help='Pulse Voltage')
-parser.add_option('--R1', action="store", default=-1, type=int, help='Voltage at R1')
-parser.add_option('--R2', action="store", default=-1, type=int, help='Voltage at R2')
+parser.add_option('--R1', action="store", default=-432, type=int, help='Voltage at R1')
+parser.add_option('--R2', action="store", default=1791, type=int, help='Voltage at R2')
 (options, args) = parser.parse_args()
 
-
-#Global variables
 SLOW_POS =  0.62
 FAST_POS = -0.62
 
-Pulse_Voltage = options.pulse
-Liner_Voltage = options.liner
-R1_Voltage = options.R1
-R2_Voltage = options.R2
+Voltages = {} # Contains all electrical values of the maching
+Voltages['pulse'] = options.pulse
+Voltages['liner'] = options.liner
+Voltages['R1'] = options.R1
+Voltages['R2'] = options.R2
 
-R1_Voltage = -432
-R2_Voltage = 1791
-
-
-if R2_Voltage < 0: # Liniar mode
-    R1_Voltage = Liner_Voltage
-    R2_Voltage = Liner_Voltage
+if Voltages['R2'] <= 0: # Liniar mode
+    Voltages['R1'] = Voltages['R1'] = Voltages['liner']
 
 
 
